eab66bb705bac98088cbf379dd168a6c91257975
pmolina90 <P.a.molina90@gmail.com>
2024-03-06 14:02:52 -0500

    added update an item test and get single item test.. but for some reason it's not working. We did make a change with coach and converting our public/component/app to a .jsx . updated routing for {app} in the test file as well. Still receiving error. sending to main to have team try

--- tests/items.test.js
+++ tests/items.test.js
@@ -2,6 +2,10 @@ const { describe, it, test, expect } = require("@jest/globals");
 const { items } = require("../server/seedData.js"); 
 const Item = require("../server/models/item.js");
 const {sequelize} = require('../server/db.js');
+// ================ Paul M additions (delete after review) ================ 
+const request = require('supertest');
+const { app } = require("../public/react/components/App.jsx")
+
 
 /*
 "name":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
@@ -37,4 +41,30 @@ describe("Unit tests for item routes", () => {
         const deletedItem = await Item.destroy({where: {id: 1}});
         expect(allItems).not.toContain(toDeleteItem);
     });
+
+    test("update an item", async () => {
+        const updatedItemData = {
+            name: "test item",
+            price: 10.00,
+            description: "Updated Item Test",
+            category: "Updated Test Category",
+            image: "Updated Test Image URL"
+        };
+        const itemToUpdate = await Item.findByPk(1);
+        await Item.update(updatedItemData, { where: { id: 1 } });
+        const updatedItem = await Item.findByPk(1); 
+        expect(updatedItem.name).toEqual(updatedItemData.name);
+        expect(updatedItem.price).toEqual(updatedItemData.price);
+        expect(updatedItem.description).toEqual(updatedItemData.description);
+        expect(updatedItem.category).toEqual(updatedItemData.category);
+        expect(updatedItem.image).toEqual(updatedItemData.image);
+    });
+
+    // test("get single item", async () => {
+    //     const expectedItemId = 1;
+    //     const expectedItem = await Item.findByPk(expectedItemId);
+    //     const res = await request(app).get(`${apiURL}/items/${expectedItemId}`); // == not to sure on filepath 
+    //     expect(res.status).toEqual(200);
+    //     expect(res.body).toMatchObject(expectedItem.toJSON()); 
+    // });
 });
